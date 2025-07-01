625578641d36e58c87c4aabed68dcd9ba1082f35
anhna <ana_mjl@hotmail.com>
2025-06-30 19:54:41 -0600

    Added and Created planets

---
+++ src/planets.jsx
@@ -0,0 +1,47 @@
+export const planets = [
+    {
+        Name: "Mercury",
+        Temperature: "430 °C",
+        Atmosphere: "Going to experience some solar winds.",
+    },
+    {
+        Name: "Venus",
+        Temperature: "457 °C",
+        Atmosphere: "I'ts going to be really hot but with some sulfuric clouds.",
+    },
+    {
+        Name: "Earth",
+        Temperature: "15 °C",
+        Atmosphere: "Some lovely hurracaines are going to hit the shores.",
+    },
+    {
+        Name: "Mars",
+        Temperature: "-80 °C",
+        Atmosphere: "Will be having some gases surrounding the planet.",
+    },
+    {
+        Name: "Jupiter",
+        Temperature: "-145 °C",
+        Atmosphere: " ",
+    },
+    {
+        Name: "Saturn",
+        Temperature: "-178 °C",
+        Atmosphere: "Put on a windbreaker because today will be windy!",
+    },
+    {
+        Name: "Uranus",
+        Temperature: "-195°C",
+        Atmosphere: "Time to get the scarf and gloves out, it will get cold.",
+    },
+    {
+        Name: "Neptune",
+        Temperature: "-214 °C",
+        Atmosphere: "It's going to rain diamonds, don't forget your umbrella.",
+    },
+    {
+        Name: "Pluto",
+        Temperature: "-233 °C",
+        Atmosphere: "tropical arctic zone will experience maximum cyclical extremes cold and is optimum for seasonal layering of clothes.",
+    },
+]
